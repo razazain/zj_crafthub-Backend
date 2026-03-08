d90992e36470d3ca9a63bd883d36d3562bc18bcd
razazain <razazain861@gmail.com>
2026-03-09 01:15:32 +0500

    debug

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -72,9 +72,8 @@ export const createProduct = async (req, res) => {
 // =============================================
 export const getProducts = async (req, res) => {
   try {
-    const { filter } = req.params;
-    const { status } = req.query;
-
+    const filter = req.params.filter || req.query.filter;
+    const status = req.query.status || req.params.status;
     const page = Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 10;
     const skip = (page - 1) * limit;
@@ -123,7 +122,7 @@ export const getProducts = async (req, res) => {
 export const getProductsByCategory = async (req, res) => {
   try {
     const { categoryId } = req.params;
-    const { status } = req.query;
+    const status  = req.query.status || req.params.status;
 
     const page = Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 10;
